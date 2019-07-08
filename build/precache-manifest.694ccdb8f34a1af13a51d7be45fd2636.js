b5158584f4e2abccbffdd95eefb58ad68d91ed8e
Brad Allen <ballen@inseq-lab.com>
2019-07-08 07:21:46 -0500

    saved my files

---
+++ build/precache-manifest.694ccdb8f34a1af13a51d7be45fd2636.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ad32b49ead3b4d1131",
+    "url": "/static/css/main.69e243bf.chunk.css"
+  },
+  {
+    "revision": "a6ad32b49ead3b4d1131",
+    "url": "/static/js/main.a6ad32b4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b21967748485e1a57bf604f1dae86ea4",
+    "url": "/index.html"
+  }
+];
